1430b0dd6d7defacfac094d45341af3c93e4c677
anncenter <anncenter@users.noreply.github.com>
2023-07-14 17:11:36 +0700

    Update circle.py
    add black color

--- circle.py
+++ circle.py
@@ -2,7 +2,7 @@ import turtle
 
 t = turtle.Turtle()
 
-for c in ['red', 'green', 'blue', 'yellow']:
+for c in ['red', 'green', 'blue', 'yellow','black']:
     t.color(c)
     t.circle(75)
     t.left(90)
