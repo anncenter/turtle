1ad298dfb214568dd7072ff4e4cc296b9fd890d9
I2\Guest-Student <yahom6gmail.com>
2023-07-18 14:14:52 +0700

    add black color

--- square.py
+++ square.py
@@ -2,7 +2,7 @@ import turtle
 
 t = turtle.Turtle()
 
-for c in ['red', 'green', 'blue', 'yellow']:
+for c in ['red', 'green', 'blue', 'yellow', 'black']:
     t.color(c)
     t.forward(75)
     t.left(90)
