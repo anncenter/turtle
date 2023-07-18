441716568848e012525bea12023fb8d9e96cbc9f
anncenter <anncenter@users.noreply.github.com>
2023-07-18 14:19:33 +0700

    Update square.py
    add pink color

--- square.py
+++ square.py
@@ -2,7 +2,7 @@ import turtle
 
 t = turtle.Turtle()
 
-for c in ['red', 'green', 'blue', 'yellow', 'black']:
+for c in ['red', 'green', 'blue', 'yellow', 'black','pink']:
     t.color(c)
     t.forward(75)
     t.left(90)
